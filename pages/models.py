076e1a0d3ab94ddf3cf2a8e860cb22c591f1b12d
ahmed5121shaban <ahmed.sh5121999@gmail.com>
2022-08-06 13:17:56 +0200

    new basic django project

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
+from unicodedata import name
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from requests import request
 
 # Create your models here.
+class pages(models.Model):
+    name = models.CharField(max_length=100)
+    tobic = models.TextField(max_length=5000)
+    
+    def __str__(self) -> str:
+        return self.name
